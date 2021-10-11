b532bc1c7a5857a0aec67ad8b05de60e5d936f6e
ShadedNasa <92326696+ShadedNasa@users.noreply.github.com>
2021-10-11 17:47:06 +0100

    Add files via upload

---
+++ ClickGUI.java
@@ -0,0 +1,33 @@
+package ozone.mod.impl;
+
+import org.lwjgl.input.Keyboard;
+
+import de.Hero.settings.Setting;
+import ozone.mod.Mod;
+import ozone.mod.ModCategory;
+
+public class ClickGUI extends Mod {
+
+	public static ClickGUI guiSettings = new ClickGUI();
+	
+	public Setting blur;
+	
+	public ClickGUI() {
+		super("ClickGUI", Keyboard.KEY_RSHIFT, ModCategory.RENDER);
+		
+		addSlider("Red", 0, 255, 209, true);
+		addSlider("Green", 0, 255, 29, true);
+		addSlider("Blue", 0, 255, 83, true);
+		
+		addCheckbox("Sound", true);
+		this.blur = addCheckbox("Blur", true);
+	}
+
+	// Just the settings for the ClickGUI
+	
+	@Override
+	public void onEnable() {
+		toggle();
+	}
+
+}
